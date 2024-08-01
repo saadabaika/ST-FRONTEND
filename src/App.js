97e0e49fcbf9c23c9c4dabe5987aa3f1d2a12302
saadabaika <157385651+saadabaika@users.noreply.github.com>
2024-08-01 14:59:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+// App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import LotSearch from './components/LotSearch';
 import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Lot Lookup</h1>
-        <Navbar />
-        <LotSearch />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Lot Lookup</h1>
+          <Navbar />
+          <div className="button-container">
+            <button className="nav-button">
+              <Link to="/lotsearch">EDA</Link>
+            </button>
+            <button className="nav-button">
+              <Link to="/lotsearch">BIDF</Link>
+            </button>
+            <button className="nav-button">
+              <Link to="/">###</Link>
+            </button>
+          </div>
+          <Routes>
+            <Route path="/lotsearch" element={<LotSearch />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
